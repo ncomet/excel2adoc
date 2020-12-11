48ad76cee0eaf01b55b41438a30e23be841878c3
Nicolas Comet <nicolas.comet@gmail.com>
2020-12-11 23:49:57 +0100

    add first tests

--- src/main/kotlin/fr/ncomet/Main.kt
+++ src/main/kotlin/fr/ncomet/Main.kt
@@ -1,5 +1,7 @@
 package fr.ncomet
 
+import org.apache.poi.hssf.usermodel.HSSFWorkbook
+import org.apache.poi.openxml4j.exceptions.OLE2NotOfficeXmlFileException
 import org.apache.poi.xssf.usermodel.XSSFWorkbook
 import picocli.CommandLine
 import picocli.CommandLine.*
@@ -14,8 +16,8 @@ val allowedFileExtensions = listOf("xlsx", "xls", "csv")
 @Command(
     name = "excel2adoc",
     mixinStandardHelpOptions = true,
-    version = ["excel2adoc 1.0"],
-    description = ["converts an .xlsx, .xls or .csv file into its asciidoc representation"]
+    version = ["excel2adoc v1.0"],
+    description = ["Converts an .xlsx, .xls or .csv file into its asciidoc representation"]
 )
 class Excel2Asciidoc : Callable<Int> {
 
@@ -24,7 +26,7 @@ class Excel2Asciidoc : Callable<Int> {
     @Parameters(index = "0", arity = "1", description=["An .xlsx, .xls or .csv file to convert to adoc"])
     lateinit var inputFile: File
 
-    @Option(names = ["-o", "--output"], paramLabel = "example.adoc", description = ["the output file name, id not provided it will default to stdout"])
+    @Option(names = ["-o", "--output"], paramLabel = "example.adoc", description = ["the output file name, if not provided it will write to stdout"])
     var outputFileName: String? = null
 
     override fun call(): Int {
@@ -32,9 +34,12 @@ class Excel2Asciidoc : Callable<Int> {
             !inputFile.exists() -> throw ParameterException(spec.commandLine(), "${inputFile.name} does not exist")
             inputFile.extension !in allowedFileExtensions -> throw ParameterException(spec.commandLine(), "${inputFile.name} needs to be of type ${allowedFileExtensions.joinToString()}")
             else -> {
-                println("file: $inputFile and name: $outputFileName")
                 FileInputStream(inputFile).use {
-                    val xssfWorkbook = XSSFWorkbook(it)
+                    val workBook = try {
+                        XSSFWorkbook(it)
+                    } catch (e: OLE2NotOfficeXmlFileException) {
+                        HSSFWorkbook(it)
+                    }
                 }
                 return ExitCode.OK
             }
@@ -42,4 +47,6 @@ class Excel2Asciidoc : Callable<Int> {
     }
 }
 
-fun main(args: Array<String>): Unit = exitProcess(CommandLine(Excel2Asciidoc()).execute(*args))
+internal fun command(args: Array<String>) = CommandLine(Excel2Asciidoc()).execute(*args)
+
+fun main(args: Array<String>): Unit = exitProcess(command(args))
--- src/test/kotlin/fr/ncomet/MainTest.kt
+++ src/test/kotlin/fr/ncomet/MainTest.kt
@@ -1,16 +1,34 @@
 package fr.ncomet
 
+import com.ginsberg.junit.exit.ExpectSystemExitWithStatus
 import io.quarkus.test.junit.QuarkusTest
-import io.restassured.RestAssured.given
-import org.hamcrest.CoreMatchers.`is`
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 
 @QuarkusTest
 class MainTest {
 
     @Test
-    fun testMain() {
+    @ExpectSystemExitWithStatus(2)
+    fun `should exit(2) if file does not exists`() {
+        main(arrayOf("src/test/resources/unknown.xlsx"))
+    }
 
+    @Test
+    @ExpectSystemExitWithStatus(2)
+    fun `should exit(2) if file extension is not allowed`() {
+        main(arrayOf("src/test/resources/file.test"))
+    }
+
+    @Test
+    fun `should load an xlsx file`() {
+        command(arrayOf("src/test/resources/file.xlsx"))
+    }
+
+    @Test
+    @Disabled
+    fun `should load an xls file`() {
+        command(arrayOf("src/test/resources/file.xls"))
     }
 
 }
