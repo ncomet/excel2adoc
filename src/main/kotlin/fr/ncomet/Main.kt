633183c7ed6b6e9b9a88a690da32dfc984138f33
Nicolas Comet <nicolas.comet@gmail.com>
2020-12-13 19:31:12 +0100

    first implementation on a single sheet

--- src/main/kotlin/fr/ncomet/Main.kt
+++ src/main/kotlin/fr/ncomet/Main.kt
@@ -2,17 +2,22 @@ package fr.ncomet
 
 import org.apache.poi.hssf.usermodel.HSSFWorkbook
 import org.apache.poi.openxml4j.exceptions.OLE2NotOfficeXmlFileException
+import org.apache.poi.ss.usermodel.Cell
 import org.apache.poi.ss.usermodel.CellType.*
+import org.apache.poi.ss.usermodel.Row
 import org.apache.poi.xssf.usermodel.XSSFWorkbook
 import picocli.CommandLine
 import picocli.CommandLine.*
 import picocli.CommandLine.Model.CommandSpec
 import java.io.File
 import java.io.FileInputStream
+import java.lang.System.lineSeparator
 import java.util.concurrent.Callable
 import kotlin.system.exitProcess
 
 val allowedFileExtensions = listOf("xlsx", "xls", "csv")
+
+fun main(args: Array<String>): Unit = exitProcess(command(args))
 
 @Command(
     name = "excel2adoc",
@@ -23,7 +28,7 @@ val allowedFileExtensions = listOf("xlsx", "xls", "csv")
 class Excel2Asciidoc : Callable<Int> {
 
     @Spec
-    lateinit var spec: CommandSpec
+    lateinit var out: CommandSpec
 
     @Parameters(index = "0", arity = "1", description = ["An .xlsx, .xls or .csv file to convert to adoc"])
     lateinit var inputFile: File
@@ -35,11 +40,24 @@ class Excel2Asciidoc : Callable<Int> {
     )
     var outputFileName: String? = null
 
+    @Option(
+        names = ["-n", "--no-headers"],
+        description = ["disables interpretation of first row as header"]
+    )
+    var noHeaders: Boolean = false
+
+    @Option(
+        names = ["-s", "--sheet"],
+        paramLabel = "2",
+        description = ["sheet number, starting at 1"]
+    )
+    var sheetNumber: Int = 1
+
     override fun call(): Int {
         when {
-            !inputFile.exists() -> throw ParameterException(spec.commandLine(), "${inputFile.name} does not exist")
+            !inputFile.exists() -> throw ParameterException(out.commandLine(), "${inputFile.name} does not exist")
             inputFile.extension !in allowedFileExtensions -> throw ParameterException(
-                spec.commandLine(),
+                out.commandLine(),
                 "${inputFile.name} needs to be of type ${allowedFileExtensions.joinToString()}"
             )
             else -> {
@@ -49,27 +67,58 @@ class Excel2Asciidoc : Callable<Int> {
                     } catch (e: OLE2NotOfficeXmlFileException) {
                         HSSFWorkbook(it)
                     }
-                    workBook.getSheetAt(0).getRow(0).cellIterator().forEach { cell ->
-                        spec.commandLine().out.println(
-                            when (cell.cellType) {
-                                _NONE -> ""
-                                NUMERIC -> cell.numericCellValue.toString()
-                                STRING -> cell.stringCellValue
-                                FORMULA -> ""
-                                BLANK -> ""
-                                BOOLEAN -> cell.booleanCellValue.toString()
-                                ERROR -> cell.errorCellValue.toString()
-                                null -> ""
-                            }
-                        )
-                    }
+                    val sheet = workBook.getSheetAt(sheetNumber - 1)
+                    val rows = sheet.toList().takeLastWhile { row -> row.isNotEmpty }
+                    out.tableSeparator()
+                    rows.printHeader()
+                    rows.printContentAfterHeader()
+                    out.tableSeparator()
+                    out.newLine()
                 }
                 return ExitCode.OK
             }
         }
     }
+
+    private fun List<Row>.printHeader() {
+        val header = first()
+        out.println(
+            header.cellIterator()
+                .asSequence()
+                .map(renderCell)
+                .joinToString(prefix = "|", separator = " |")
+        )
+    }
+
+    private fun List<Row>.printContentAfterHeader() {
+        val content = drop(1)
+        out.println(
+            content.joinToString(separator = lineSeparator() + lineSeparator()) { row ->
+                row.cellIterator()
+                    .asSequence()
+                    .map(renderCell)
+                    .joinToString(prefix = "|", separator = "${lineSeparator()}|")
+            })
+    }
+}
+
+val renderCell: (Cell) -> String = { cell ->
+    when (cell.cellType) {
+        _NONE -> ""
+        NUMERIC -> cell.numericCellValue.toString()
+        STRING -> cell.stringCellValue
+        FORMULA -> ""
+        BLANK -> ""
+        BOOLEAN -> cell.booleanCellValue.toString()
+        ERROR -> cell.errorCellValue.toString()
+        else -> ""
+    }
 }
 
 internal fun command(args: Array<String>) = CommandLine(Excel2Asciidoc()).execute(*args)
 
-fun main(args: Array<String>): Unit = exitProcess(command(args))
+private fun CommandSpec.tableSeparator() = commandLine().out.println("|===")
+private fun CommandSpec.println(s: String) = commandLine().out.println(s)
+private fun CommandSpec.newLine() = commandLine().out.println(lineSeparator())
+private val Cell.isNotEmpty get() = cellType != BLANK
+private val Row.isNotEmpty get() = cellIterator().asSequence().any { it.isNotEmpty }
--- src/test/kotlin/fr/ncomet/MainTest.kt
+++ src/test/kotlin/fr/ncomet/MainTest.kt
@@ -38,26 +38,90 @@ class MainTest {
         command(arrayOf("src/test/resources/file.xlsx"))
 
         assertThat(out.capturedLines().joinToString(System.lineSeparator())).isEqualTo(
-            """
+            """|===
+            :|First Name |Last Name |Gender |Country |Age |Date |Id |Number
+            :|Dulce
+            :|Abril
+            :|Female
+            :|United States
+            :|32.0
+            :|15/10/2017
+            :|1562.0
+            :|32.3
+            :
+            :|Mara
+            :|Hashimoto
+            :|Female
+            :|Great Britain
+            :|25.0
+            :|16/08/2016
+            :|1582.0
+            :|23.0
+            :
+            :|Philip
+            :|Gent
+            :|Male
+            :|France
+            :|36.0
+            :|21/05/2015
+            :|2587.0
+            :|15.5
+            :
+            :|Kathleen
+            :|Hanner
+            :|Female
+            :|United States
+            :|25.0
+            :|15/10/2017
+            :|3549.0
+            :|23.5445
+            :
+            :|Nereida
+            :|Magwood
+            :|Female
+            :|United States
+            :|58.0
+            :|16/08/2016
+            :|2468.0
+            :|11.0
+            :
+            :|Gaston
+            :|Brumm
+            :|Male
+            :|United States
+            :|24.0
+            :|21/05/2015
+            :|2554.0
+            :|0.4455
+            :
+            :|Etta
+            :|Hurn
+            :|Female
+            :|Great Britain
+            :|56.0
+            :|15/10/2017
+            :|3598.0
+            :|23.0
+            :
+            :|Earlean
+            :|Melgar
+            :|Female
+            :|United States
+            :|27.0
+            :|16/08/2016
+            :|2456.0
+            :|14.0
+            :
+            :|Vincenza
+            :|Weiland
+            :|Female
+            :|United States
+            :|40.0
+            :|21/05/2015
+            :|6548.0
+            :|6.0
             :|===
-            :|Header 1 |Header 2 |Header 3 |Header 4
-            :
-            :|Column 1, row 1
-            :|Column 2, row 1
-            :|Column 3, row 1
-            :|Column 4, row 1
-            :
-            :|Column 1, row 2
-            :|Column 2, row 2
-            :|Column 3, row 2
-            :|Column 4, row 2
-            :
-            :|Column 1, row 3
-            :|Column 2, row 3
-            :|Column 3, row 3
-            :|Column 4, row 3
-            :|===
-:""".trimMargin(":")
+""".trimMargin(":")
         )
     }
 
